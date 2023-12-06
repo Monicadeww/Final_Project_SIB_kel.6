ae5fed3967357dc3dc1dba9c9d5f566d5fdea19f
Indah Monica Dewi <144128950+Monicadeww@users.noreply.github.com>
2023-12-07 06:53:40 +0700

    Delete Metabase/#Measure the conversion rate of orders w.sql

--- Metabase/#Measure the conversion rate of orders w.sql
+++
@@ -1,7 +0,0 @@
-#Measure the conversion rate of orders with and without coupons
-SELECT
-    COUNT(DISTINCT CASE WHEN oi.coupon_id IS NOT NULL THEN oi.order_id END) AS orders_with_coupons,
-    COUNT(DISTINCT oi.order_id) AS total_orders,
-    COUNT(DISTINCT CASE WHEN oi.coupon_id IS NOT NULL THEN oi.order_id END) * 1.0 / COUNT(DISTINCT oi.order_id) AS conversion_rate
-FROM
-    order_items oi;
